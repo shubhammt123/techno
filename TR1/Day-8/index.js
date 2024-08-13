3e22dda267a76e11b572c421938e5dd19c54c8f8
shubham <shubhammt123@gmail.com>
2024-08-13 11:55:40 +0530

    Js Operator

---
+++ TR1/Day-8/index.js
@@ -0,0 +1,177 @@
+// var num1 = 20;
+
+// console.log("Hello Techno");
+
+// alert("Hello Techno");
+
+// var , let , const
+
+// primitive types - string , number , boolean , undefined , symbol , bigint 
+// reference or object typed data types -- array , objects , date , null
+
+// var firstVar = "I'm a full stack 'developer'";
+// console.log(firstVar)
+// console.log(typeof firstVar);
+
+
+// var firstVar = -20.01;
+// console.log(firstVar);
+// console.log(typeof firstVar);
+
+
+// var firstVar = false;
+// console.log(firstVar);
+// console.log(typeof firstVar);
+
+
+// var firstVar = undefined;
+// console.log(firstVar);
+// console.log(typeof firstVar);
+
+
+// var firstVar = [20,30,"Hello",true,undefined];
+// console.log(firstVar);
+// console.log(typeof firstVar);
+
+// var firstVar = null;
+// console.log(firstVar);
+// console.log(typeof firstVar);
+
+// var firstVar = {
+//     firstName : "Shubham",
+//     lastName : "Jain"
+// };
+// console.log(firstVar);
+// console.log(typeof firstVar);
+
+// var date = new Date();
+// console.log(date);
+// console.log(typeof date)
+
+// [{},{},{}] - json
+
+// var firstVar = "Hello Techno";
+// firstVar = [20,30];
+// firstVar = {
+//     firstName : "shubham",
+//     lastName : "Jain"
+// };
+
+// firstVar = true;
+
+// console.log(firstVar);
+
+// var , let , const
+
+
+
+// var num1 = 30;
+
+
+// var num1 = {
+//     firstName : "Shubham"
+// }
+
+// console.log(num1)
+
+
+// let num2 = 20;
+
+var num1 = 20;
+
+var num1 = 30;
+// variable defined with var can be redecleared or can be re assigned
+
+
+let num2 = 50;
+ num2 = 100;
+
+// variable defined with let cannot be redecleared but can be re assigned
+
+// const num3 = 100;
+// num3 = 1000;
+
+// variable defined with const cannot be redecleared or re assigned
+
+// Operators
+
+// Arithmatic operators
+
+// + ,  - , * , /
+
+let a = 30;
+let b = 20;
+
+let add = a + b;
+let  sub = a-b;
+let mul = a*b;
+let div = a/b
+// console.log(add);
+// console.log(sub);
+// console.log(mul);
+// console.log(div);
+
+let firstName = "shubham";
+let lastName = "Jain";
+
+// console.log(firstName+ " " +lastName);
+
+// console.log(a**b);
+// console.log(a%b);
+
+// console.log(5/"5");
+// console.log("5"/5);
+
+// Addition Assignements
+
+let x = 5;
+
+// x = x + 10;
+x += 10;
+
+// console.log(x);
+
+let str = "shubham";
+let conStr = +str;
+// console.log( conStr);
+// console.log(typeof conStr);
+
+// console.log("shubham" - 10);
+
+let str1 = "5";
+let str2 = +str1;
+
+// console.log(5+"5");
+
+
+let y = 10;
+// y -= 3;
+// y *= 3;
+// y /= 3;
+// y %= 3;
+// y **= 3;
+
+// console.log(y);
+
+// Comparision operator
+
+let num4 = 10;
+let num5 = "10";
+
+console.log(num4 == num5);
+console.log(num4 === num5);
+
+
+// Logical operator
+
+// AND(&&) OR(||)
+
+let isLoggedIn = 0; 
+let isAdmin = 10; 
+
+console.log("Is the user logged in and an admin?", isLoggedIn && isAdmin);
+
+console.log(10 || 1000);
+
+
+
