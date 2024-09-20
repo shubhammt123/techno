42e7aa32c65148804803df9ee59d9a00b481465b
shubham <shubhammt123@gmail.com>
2024-09-20 10:26:19 +0530

    frontend validations

--- TR1/project/frontend/src/components/Signup.jsx
+++ TR1/project/frontend/src/components/Signup.jsx
@@ -1,8 +1,24 @@
+import { zodResolver } from '@hookform/resolvers/zod';
 import React from 'react'
 import { useForm } from 'react-hook-form'
+import { z } from 'zod';
 
 const Signup = () => {
-    const { register , handleSubmit } = useForm();
+    const validationSchema = z.object({
+        name : z.string().min(1,"Name is required").max(40,"Name cannot exceed 40 charater"),
+        email : z.string().min(1,"Email is required").email("Invalid Email"),
+        password : z.string().min(8, "Password must be of 8 charaters")
+        .regex(/[a-z]/,"Password must contain atleast 1 lower case letter")
+        .regex(/[A-Z]/,"Password must contain atleat 1 upper case letter")
+        .regex(/[0-9]/,"Password must contain atleast 1 number")
+        .regex(/[\w_]/,"Password must contain  atleast 1 special charater"),
+        phoneNumber : z.string().min(10,"Phone number is required")
+    })
+
+
+    const { register , handleSubmit , formState : {errors} } = useForm({
+        resolver : zodResolver(validationSchema)
+    });
     const onSubmit = (data)=>{
         console.log(data);
     }
@@ -17,19 +33,31 @@ const Signup = () => {
                     <div className='grid grid-cols-2 gap-2'>
                     <div>
                         <label className='text-gray-500 font-medium'>Name</label>
-                        <input type="text" className='block p-2 border border-gray-700 outline-none w-4/5 my-2' {...register("name")} />
+                        <input type="text" className={`block p-2 border outline-none w-4/5 my-2 ${errors.name ? ' border-red-500' : 'border-gray-700'}`} {...register("name")} />
+                        {errors.name && (
+                            <p className='text-xs text-red-500'>{errors.name.message}</p>
+                        ) }
                     </div>
                     <div>
                     <label className='text-gray-500 font-medium'>Email</label>
-                    <input type="email" className='block p-2 border border-gray-700 outline-none w-4/5 my-2' {...register("email")}  />
+                    <input type="email" className={`block p-2 border outline-none w-4/5 my-2 ${errors.email ? 'border-red-500' : "border-gray-700"}`} {...register("email")}  />
+                    {errors.email && (
+                            <p className='text-xs text-red-500'>{errors.email.message}</p>
+                        ) }
                     </div>
                     <div>
                     <label className='text-gray-500 font-medium'>Password</label>
-                    <input type="password" className='block p-2 border border-gray-700 outline-none w-4/5 my-2' {...register("password")} />
+                    <input type="password" className={`block p-2 border border-gray-700 outline-none w-4/5 my-2 ${errors.password ? 'border-red-500' : "border-gray-700"}`} {...register("password")} />
+                    {errors.password && (
+                            <p className='text-xs text-red-500'>{errors.password.message}</p>
+                        ) }
                     </div>
                     <div>
                     <label className='text-gray-500 font-medium'>Phone Number</label>
-                    <input type="number" className='block p-2 border border-gray-700 outline-none w-4/5 my-2' {...register("phoneNumber")}  />
+                    <input type="number" className={`block p-2 border border-gray-700 outline-none w-4/5 my-2 ${errors.phoneNumber ? 'border-red-500' : "border-gray-700"}`} {...register("phoneNumber")}  />
+                    {errors.phoneNumber && (
+                            <p className='text-xs text-red-500'>{errors.phoneNumber.message}</p>
+                        ) }
                     </div>
                     </div>
                     <button className='p-2 w-[90%] my-4 font-medium text-gray-600 shadow bg-sky-300 active:bg-sky-400'>Signup</button>
@@ -40,3 +68,5 @@ const Signup = () => {
 }
 
 export default Signup
+
+
