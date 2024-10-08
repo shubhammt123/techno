e1abb9fa2e811debaa73075eaf88a02ec94e8856
shubham <shubhammt123@gmail.com>
2024-10-08 22:36:35 +0530

    abc

--- TR4/1project/backend/middleware/multer.js
+++ TR4/1project/backend/middleware/multer.js
@@ -2,7 +2,7 @@ const multer = require("multer");
 
 const storage = multer.diskStorage({
     destination : (req,file,cb)=>{
-        cb(null,"./uploads/");
+        cb(null,"uploads/");
     },
     filename : (req,file,cb)=>{
         cb(null, `${Date.now()}-${file.originalname}`);
